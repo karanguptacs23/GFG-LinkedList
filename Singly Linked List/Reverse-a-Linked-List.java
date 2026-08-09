c582967929891e06d28dc1350cf5c0c25425c0f1
karanguptacs23 <karan.gupta_cs23@gla.ac.in>
2026-08-10 01:41:43 +0530

    Added reverse a linked list

---
+++ Singly Linked List/Reverse-a-Linked-List.java
@@ -0,0 +1,26 @@
+/* Structure of Linked List Node
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    Node reverseList(Node head) {
+        // code here
+        Node prev = null;
+        Node temp = head;
+        while(temp!=null){
+            Node front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+}
